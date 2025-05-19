001beb273d1fa37837992b15d7f29ce1b1c339bd
DEATHSHOOTERgd <giampaolo.delgadoo@ug.edu.ec>
2025-05-18 21:45:55 -0500

    Frontend archetype initialization.

---
+++ src/app/modules/customers/components/customer-list/customer-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Customer } from '../../interfaces/customer.interface';
+import { CustomerService } from '../../services/customer.service';
+
+@Component({
+  selector: 'customer-list',
+  standalone: true,
+  templateUrl: './customer-list.component.html',
+  styleUrl: './customer-list.component.scss'
+})
+export class CustomerListComponent implements OnInit{
+  private _customerService = inject(CustomerService);
+
+  ngOnInit(): void {
+    this._customerService.getCustomers().subscribe({
+      next: (res) => {
+        this.customers = res.data;
+      },
+      error:(err)=>{
+      }
+    });
+  }
+  
+  customers: Customer[] = [
+    {
+      id:1,
+      name:'Banco Guayaquil',
+      phone:'999999999',
+      email:'info@bancoguayaquil.com'
+    },
+    {
+      id:2,
+      name:'Banco Bolivariano',
+      phone:'999999999',
+      email:'info@bolivariano.com'
+    }];
+}
