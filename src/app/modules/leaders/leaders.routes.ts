1bc5cd2ccbe1212aa6c4f7810ac21706294e258d
jvivas-rock <jvivas.trecool@gmail.com>
2025-06-03 16:05:41 -0500

    Feature Leader/Person Create

--- src/app/modules/leaders/leaders.routes.ts
+++ src/app/modules/leaders/leaders.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from "@angular/router";
 import { ManageLeadersPage } from "./pages/manage-leaders/manage-leaders.page";
 import { ListLeadersPage } from "./pages/list-leaders/list-leaders.page";
 
-export const PoepleRoutes: Routes = [
+export const LeaderRoutes: Routes = [
   {
     path: '',
     component: ListLeadersPage
