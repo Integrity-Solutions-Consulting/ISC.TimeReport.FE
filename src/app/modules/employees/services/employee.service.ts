ea22b225ec9b2b4e2eada510dd578dc9aad80ab9
jvivas-rock <jvivas.trecool@gmail.com>
2025-06-13 15:23:59 -0500

    Feature Create Employee with Persons and Get Employees

--- src/app/modules/employees/services/employee.service.ts
+++ src/app/modules/employees/services/employee.service.ts
@@ -1,18 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { SuccessResponse } from '../../../shared/interfaces/response.interface';
+import { Employee } from '../interfaces/employee.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  private apiUrl = 'https://tu-api.com/employees';
+  urlBase: string = environment.URL_BASE;
 
   constructor(private http: HttpClient) { }
 
-  createEmployee(employeeData: any): Observable<any> {
+  getEmployees():Observable<Employee[]>{
+          return this.http.get<Employee[]>(
+              `${this.urlBase}/api/employee/get`
+            );
+      }
+
+  createEmployee(employeeData: Employee): Observable<SuccessResponse<Employee>> {
     console.log(employeeData);
-    return this.http.post(this.apiUrl, employeeData);
+    return this.http.post<SuccessResponse<Employee>>(`${this.urlBase}/api/employee/create`, employeeData);
   }
 }
