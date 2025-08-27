4a819d314783a98aa6e103322161d37c6bef0002
jvivas-rock <jvivas.trecool@gmail.com>
2025-08-27 16:11:58 -0500

    On Production

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
     URL_API:'https://github.com',
     //URL_BASE: 'http://147.93.181.146:8082',
-    //URL_BASE: 'https://api.timereport.integritysolutions.com.ec',
-    URL_BASE: 'http://192.168.100.149/time-report.be',
+    URL_BASE: 'https://api.timereport.integritysolutions.com.ec',
+    //URL_BASE: 'http://192.168.100.149/time-report.be',
     URL_TEST: 'https://localhost:44392',
     URL_INV: 'https://isc-inventory-back-api.onrender.com/api/v1'
 };
