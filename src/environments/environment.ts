15eb619f2d4b8a03fd56f756cad5aa6bc12f3f8e
jvivas-rock <jvivas.trecool@gmail.com>
2025-09-03 17:09:30 -0500

    Changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,9 @@
 export const environment = {
     URL_API:'https://github.com',
-    URL_BASE: 'URL_BASE_PLACEHOLDER2',
+    //URL_BASE: 'URL_BASE_PLACEHOLDER2',
     //URL_BASE: 'http://147.93.181.146:8082',
     //URL_BASE: 'https://192.168.100.149/time-report.be',
+    URL_BASE: 'https://api.timereport.integritysolutions.com.ec/',
     URL_TEST: 'https://localhost:44392',
     URL_INV: 'URL_INV_PLACEHOLDER2'
 };
